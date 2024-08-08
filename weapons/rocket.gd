a31f7871f04dd6383fb6197dede04b31a6a109a7
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-08-08 15:52:47 -0500

    Kind of working?

--- weapons/rocket.gd
+++ weapons/rocket.gd
@@ -1,6 +1,5 @@
 extends Node3D
 @onready var animation_player: AnimationPlayer = $rocket/AnimationPlayer
-@onready var area_3d: Area3D = $rocket/booster/engine/Area3D
 
 @onready var explosion = preload("res://weapons/explosion.tscn")
 @onready var HOLE = preload("res://weapons/hole.tscn")
@@ -14,14 +13,14 @@ func fire() -> void:
 	animation_player.play("boosterAction")
 
 
-func explode() -> void:
+func explode(pos) -> void:
 	exploded = true
 	animation_player.pause()
 	$rocket.hide()
 
 	var efx = explosion.instantiate()
 	add_child(efx)
-	efx.global_position = $rocket/booster/engine/Cylinder.global_position
+	efx.global_position = pos
 	efx.global_rotation.x = 0
 	efx.global_rotation.z = 0
 	efx.explode()
@@ -30,34 +29,25 @@ func explode() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if entered_body:
 		if not exploded:
-			print("area hit!")
 			var space_state = get_world_3d().direct_space_state
 			var from = previous_position
-			var to = $rocket/booster/engine/Cylinder.global_position
-			var query = PhysicsRayQueryParameters3D.create(from, to, 0xFFFFFFFF, [self])
+			var to = $rocket/booster/engine/Cylinder.global_transform.origin
+			var query = PhysicsRayQueryParameters3D.create(from, to)
+			query.set_exclude([self])
 			query.collide_with_bodies = true
 			var result = space_state.intersect_ray(query)
-			previous_position = $rocket/booster/engine/Cylinder.global_position
+			previous_position = $rocket/booster/engine/Cylinder.global_transform.origin
 
-			if result:
+			if result and result.collider is not CharacterBody3D:
 				print("raycast hit!")
 				exploded = true
 				var hole = HOLE.instantiate()
-				entered_body.add_child(hole)
+				print(result)
+				result.collider.add_child(hole)
 				hole.global_position = result.get("position") # $rocket/booster/engine/Cylinder.global_position
 				var normal = result.get("normal")
 				look_at(global_transform.origin + normal, Vector3.UP)
 				if normal != Vector3.UP and normal != Vector3.DOWN:
 					self.rotate_object_local(Vector3(1, 0, 0), 90)
-				explode()
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	entered_body = body
-
-
-func _on_area_3d_body_exited(body: Node3D) -> void:
-	if body == entered_body:
-		entered_body = null
+				explode(result.position)
